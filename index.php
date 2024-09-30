495749113b80d1057e0c3fa17e7f7bd031c0ffe8
GON LUNA <gon.luna234@gmail.com>
2024-09-30 16:04:18 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" class="post"></form>
+    Username:<br>
+    <input type="text" name = "username"><br>
+    Password:<br>
+    <input type="text" name="password"><br>
+    <input type="submit" name="login" value="login">
+</body>
+</html>
